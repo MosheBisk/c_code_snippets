2c09ee6cb3285fa32d8c8c4a864871e6818e83e7
Moshe A. Bisk <moshebisk@gmail.com>
2022-07-20 17:54:24 +0300

    Commented unlimited_str_and_clear_buffer file.

--- unlimited_str_and_clear_buffer.c
+++ unlimited_str_and_clear_buffer.c
@@ -9,17 +9,17 @@ int main()
     do
     {
         fgets(tempHolder, 10, stdin);
-        if (strchr(tempHolder, '\n'))
+        if (strchr(tempHolder, '\n')) // if the string has a newline character
         {
-            *(strchr(tempHolder, '\n')) = '\0';
+            *(strchr(tempHolder, '\n')) = '\0'; // replace the newline character with a null terminator
         }
-        fullStr = (char*) realloc(fullStr, strlen(fullStr ? fullStr : "") + strlen(tempHolder));
-        strcat(fullStr, tempHolder);
+        fullStr = (char*) realloc(fullStr, strlen(fullStr ? fullStr : "") + strlen(tempHolder)); // add memory space to the dest string
+        strcat(fullStr, tempHolder); // append the temp string to the dest string
         printf("test1: %s\n", fullStr);
-    } while (strlen(tempHolder) == 9);
+    } while (strlen(tempHolder) == 9); // if the temp string is full, keep looping
     
     printf("test2: %s\n", fullStr);
     free(fullStr);
-
+    fullStr = NULL;
     return 0;
 }
